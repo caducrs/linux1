abfd40bf74b252c38c1aa894034a527742baf98f
Carlos Eduardo <108847033+caducrs@users.noreply.github.com>
2024-12-11 22:11:30 -0300

    iacex1.sh

--- iacex1.sh
+++ iacex1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# DIO Bootcamp 
 
 echo "Inicializando o Script:"
 
@@ -8,41 +7,41 @@ echo "Criando os diretórios:"
 
 mkdir /publico
 mkdir /adm
-mkdir /ven
+mkdir /vend
 mkdir /sec
 
 echo "Grupos sendo Criados:"
 
 groupadd GRP_ADM
-groupadd GRP_VEN
+groupadd GRP_VEND
 groupadd GRP_SEC
 
 echo "Criando os usuários e atribuindo aos grupos:"
 
-useradd ana -c "Ana" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
-useradd bruno -c "Bruno" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
-useradd clara -c "Clara" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
+useradd ana -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_ADM
+useradd bruno -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_ADM
+useradd clara -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_ADM
 
-useradd daniel -c "Daniel" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
-useradd elena -c "Elena" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
-useradd felipe -c "Felipe" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
+useradd daniel -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_VEND
+useradd elena -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_VEND
+useradd felipe -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_VEND
 
-useradd gabi -c "Gabi" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
-useradd hugo -c "Hugo" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
-useradd iris -c "Iris" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
+useradd gabi -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_SEC
+useradd hugo -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_SEC
+useradd iris -m -s /bin/bash -p $(openssl passwd -crypt S3311224) -G GRP_SEC
 
 
 echo "Definindo os donos e os grupos dos diretórios:"
 
 chown root:GRP_ADM /adm
-chown root:GRP_VEN /ven
+chown root:GRP_VEN /vend
 chown root:GRP_SEC /sec
 
 echo "Definindo as permissões de usuário nos diretórios:"
 
 chmod 777 /publico
 chmod 770 /adm
-chmod 770 /ven
+chmod 770 /vend
 chmod 770 /sec
 
-echo "Script finalizado com Sucesso!!"
+echo "Sucesso @caducrs!!"
