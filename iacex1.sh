203c6ceb1732d215f2cf797c9ec4b30a5e85d2f3
Carlos Eduardo <108847033+caducrs@users.noreply.github.com>
2024-12-11 21:47:42 -0300

    iacex1.sh

--- iacex1.sh
+++ iacex1.sh
@@ -19,17 +19,17 @@ groupadd GRP_SEC
 
 echo "Criando os usuários e atribuindo aos grupos:"
 
-useradd ana -c "Ana" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
-useradd bruno -c "Bruno" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
-useradd clara -c "Clara" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
+useradd ana -c "Ana" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
+useradd bruno -c "Bruno" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
+useradd clara -c "Clara" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_ADM
 
-useradd daniel -c "Daniel" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_VEN
-useradd elena -c "Elena" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_VEN
-useradd felipe -c "Felipe" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_VEN
+useradd daniel -c "Daniel" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
+useradd elena -c "Elena" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
+useradd felipe -c "Felipe" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_VEN
 
-useradd gabi -c "Gabi" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_SEC
-useradd hugo -c "Hugo" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_SEC
-useradd iris -c "Iris" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_SEC
+useradd gabi -c "Gabi" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
+useradd hugo -c "Hugo" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
+useradd iris -c "Iris" -s /bin/bash -m -p $(openssl passwd -6 331122) -G GRP_SEC
 
 
 echo "Definindo os donos e os grupos dos diretórios:"
